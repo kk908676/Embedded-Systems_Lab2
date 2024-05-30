d41e0fc91b4f1d3d93043f9a0cbcdf9fefdf5ac8
kk908676 <kk908676@gmail.com>
2024-05-30 13:26:44 +0800

    first commit

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/projdefs.h \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/portable.h \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/list.h
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/FreeRTOS.h:
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/projdefs.h:
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/portable.h:
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/zephy/Desktop/Embedded\ Systems/lab2/lab2/FreeRTOS/include/list.h:
